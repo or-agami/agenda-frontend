e332414457fd0ba2c41f21a45727de99ed2623d3
TalBenAtiya <benatia.tal@gmail.com>
2022-09-14 22:26:32 +0200

    working on index header design

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -3,17 +3,7 @@ import { NavLink } from 'react-router-dom'
 export const AppHeader = () => {
 
   return (<section className="app-header">
-    <header className="main-layout app-header">
-      <div className="logo">
-        <h1>Toys</h1>
-      </div>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/board">Board</NavLink>
-        <NavLink to="/home">Board Home</NavLink>
-        <NavLink to="/login">login</NavLink>
-      </nav>
-    </header>
+
   </section>
   )
 }
