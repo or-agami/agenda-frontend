4dcde21aad4a0192cdaeaa68987d22b30c653d69
TalBenAtiya <benatia.tal@gmail.com>
2022-09-15 18:02:05 +0200

    home page design advanced

---
+++ src/cmps/logo.jsx
@@ -0,0 +1,8 @@
+import logoIcon from '../assets/icons/agenda-logo-color.svg'
+
+export const Logo = () => {
+    return <div className="logo">
+        <img src={logoIcon} />
+        <h1>agenda</h1>
+    </div>
+}
