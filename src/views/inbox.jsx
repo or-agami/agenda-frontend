015fceeb6dde76aaa3dbc55b5996cc13946ae342
Gal Wender <galwender@gmail.com>
2022-09-15 18:26:23 +0300

    minor

--- src/views/inbox.jsx
+++ src/views/inbox.jsx
@@ -7,7 +7,7 @@ export const Inbox = () => {
             <h1>Inbox</h1>
             <p>Catch up on updates from all your boards. <Link>Learn more</Link></p>
         </div>
-        <span className="inbox-toggle-mode"><a class="active">Open</a> / <a className="" id="inbox-all-updates-link">All Updates</a></span>
+        <span className="inbox-toggle-mode"><a className="active">Open</a> / <a className="" id="inbox-all-updates-link">All Updates</a></span>
         <img src={fistBumpGif} width={320} height={320} alt="" />
         <p>Fist Bumb!</p>
         <p>Your inbox is empty, We'll let you know when we get news</p>
