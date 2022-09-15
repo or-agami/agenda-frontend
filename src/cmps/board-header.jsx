3ec47f0c9110de71e48f870e184a41a64fd21289
or-agami <oragami0@gmail.com>
2022-09-15 14:06:07 +0300

    board-header update

--- src/cmps/board-header.jsx
+++ src/cmps/board-header.jsx
@@ -1,23 +1,30 @@
 import { ReactComponent as InviteSvg } from '../assets/icons/invite.svg'
 import { ReactComponent as BoardMenuSvg } from '../assets/icons/board-menu.svg'
 import { ReactComponent as HomeSvg } from '../assets/icons/home.svg'
+import {BiSearch} from 'react-icons/bi'
 
 export const BoardHeader = () => {
   return (
     <section className="board-header">
-      <div className="board-info">
+      <div className="flex board-info">
         <h1 className="title">New Board</h1>
-        <button className="btn btn-svg invite">
-          <InviteSvg />
-          Invite
-        </button>
-        <button className="btn btn-svg invite">
-          <BoardMenuSvg />
-        </button>
+        <div className="btns-container">
+          <button className="btn btn-svg invite">
+            <InviteSvg />
+            Invite
+          </button>
+          <button className="btn btn-svg invite">
+            <BoardMenuSvg />
+          </button>
+        </div>
       </div>
-      <div className="board-nav">
-        <HomeSvg />
-        Main Table
+      <div className="flex board-nav">
+        <div className="btns-container">
+          <button className="btn btn-ri search">
+            <BiSearch />
+            Search
+          </button>
+        </div>
       </div>
     </section>
   )
