2c2499d389bcf90ba438f1627bc0b1a78830b8f8
Gal Wender <galwender@gmail.com>
2022-09-16 11:01:54 +0300

    working on group preview

--- src/cmps/group-preview.jsx
+++ src/cmps/group-preview.jsx
@@ -11,28 +11,48 @@ export const GroupPreview = ({ group }) => {
   }
   console.log('group:', group)
   const { title } = group
-  return (
-    <div className="group">
-      <h1 className="title">{title}</h1>
-      <ul className="task-list">
-        <li className="clean-list cmps">
-          <span className="task">Task</span>
-        </li>
-        <li className="column-header column-header-developer">
-          <span className="developer">Developer</span>
-        </li>
-        <li className="column-header column-header-status">
-          <span className="status">status</span>
-        </li>
-        <li className="column-header column-header-date">
-          <span className="date">Date</span>
-        </li>
-      </ul>
-      <div>
-        {group.tasks.map((task) => (
-          <TaskPreview key={task.id} task={task} />
-        ))}
-      </div>
-    </div>
-  )
+  return <ul className="group-preview">
+    <li className="group-header-color">
+
+    </li>
+    <li className="group-header-name">
+      <ArrowRightSvg/>
+      <h4>{title}</h4>
+    </li>
+    <li className="group-header-task">
+      
+    </li>
+    <li className="group-header-developer">
+
+    </li>
+    <li className="group-header-status">
+
+    </li>
+    <li className="group-header-date">
+
+    </li>
+  </ul>
+    // <div className="group">
+    //   <h1 className="title">{title}</h1>
+    //   <ul className="task-list">
+    //     <li className="clean-list cmps">
+    //       <span className="task">Task</span>
+    //     </li>
+    //     <li className="column-header column-header-developer">
+    //       <span className="developer">Developer</span>
+    //     </li>
+    //     <li className="column-header column-header-status">
+    //       <span className="status">status</span>
+    //     </li>
+    //     <li className="column-header column-header-date">
+    //       <span className="date">Date</span>
+    //     </li>
+    //   </ul>
+    //   <div>
+    //     {group.tasks.map((task) => (
+    //       <TaskPreview key={task.id} task={task} />
+    //     ))}
+    //   </div>
+    // </div>
+  
 }
