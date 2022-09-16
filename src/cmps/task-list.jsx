672ff1f865340d51185e62aa0744045bc4213197
Gal Wender <galwender@gmail.com>
2022-09-16 22:19:00 +0300

    working on task remove and task menu

--- src/cmps/task-list.jsx
+++ src/cmps/task-list.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import { ReactComponent as BoardMenu } from '../assets/icons/board-menu.svg'
 import { TaskAdd } from './task-add'
+import { TaskMenu } from './task-menu'
 import { TaskPreview } from './task-preview'
-export const TaskList = ({ group ,boardId}) => {
-    const [isTaskMenuOpen,setIsTaskMenuOpen] = useState(false)
-    const toggleTaskMenu = () => {
-        
-    }
+
+export const TaskList = ({ group, boardId }) => {
+    const [isTaskMenuOpen, setIsTaskMenuOpen] = useState(false)
+
     return <section className="task-list">
         {group.tasks.map((task) => (
             <div key={task.id} className='task-preview-container'>
-                <button className='btn btn-svg btn-task-menu' onClick={(ev)=>toggleTaskMenu(ev)}><BoardMenu /></button>
-                <div className={``}></div>
+                <button className='btn btn-svg btn-task-menu' onClick={() => setIsTaskMenuOpen(!isTaskMenuOpen)}><BoardMenu /></button>
                 <TaskPreview task={task} />
             </div>
         ))}
-        <TaskAdd 
-        groupId={group.id} 
-        boardId={boardId}
+        {isTaskMenuOpen && <TaskMenu />}
+        <TaskAdd
+            groupId={group.id}
+            boardId={boardId}
         />
     </section>
 }
---
+++ src/cmps/task-menu.jsx
@@ -0,0 +1,8 @@
+export const TaskMenu = () => {
+    return <section className="task-menu">
+        <div className="group-color-icon-container">
+            <div className="group-color-icon"></div>
+            <button>Change group color</button>
+        </div>
+    </section>
+}
