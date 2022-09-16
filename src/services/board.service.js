9e3dde1019aceaa1a1ed7855df7dd69a8952e203
or-agami <oragami0@gmail.com>
2022-09-16 18:26:58 +0300

    fix get by id in dev

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -9,7 +9,287 @@ export const boardService = {
   remove,
 }
 //?- Dev:
-const gBoards = require("../data/board.json")
+const gBoards = [
+  {
+    "_id": "b101",
+    "title": "Robot dev proj",
+    "archivedAt": 1589983468418,
+    "createdAt": 1589983468418,
+    "createdBy": {
+      "_id": "u101",
+      "fullname": "Abi Abambi",
+      "imgUrl": "http://some-img"
+    },
+    "style": {},
+    "labels": [
+      {
+        "id": "l101",
+        "title": "Done",
+        "color": "--status-clr1"
+      },
+      {
+        "id": "l102",
+        "title": "Progress",
+        "color": "--status-clr1"
+      }
+    ],
+    "members": [
+      {
+        "_id": "u101",
+        "fullname": "Tal Tarablus",
+        "imgUrl": "https://www.google.com"
+      }
+    ],
+    "groups": [
+      {
+        "id": "g101",
+        "title": "Group 1",
+        "archivedAt": 1589983468418,
+        "tasks": [
+          {
+            "id": "c101",
+            "title": "Replace logo"
+          },
+          {
+            "id": "c102",
+            "title": "Add Samples"
+          }
+        ],
+        "style": {}
+      },
+      {
+        "id": "g102",
+        "title": "Group 2",
+        "tasks": [
+          {
+            "id": "c103",
+            "title": "Do that",
+            "archivedAt": 1589983468418
+          },
+          {
+            "id": "c104",
+            "title": "Help me",
+            "status": "in-progress",
+            "description": "description",
+            "comments": [
+              {
+                "id": "ZdPnm",
+                "txt": "also @yaronb please CR this",
+                "createdAt": 1590999817436,
+                "byMember": {
+                  "_id": "u101",
+                  "fullname": "Tal Tarablus",
+                  "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                }
+              }
+            ],
+            "checklists": [
+              {
+                "id": "YEhmF",
+                "title": "Checklist",
+                "todos": [
+                  {
+                    "id": "212jX",
+                    "title": "To Do 1",
+                    "isDone": false
+                  }
+                ],
+                "createdAt": 1590999817436,
+                "byMember": {
+                  "_id": "u101",
+                  "fullname": "Tal Tarablus",
+                  "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                }
+              }
+            ],
+            "memberIds": [
+              "u101"
+            ],
+            "labelIds": [
+              "l101",
+              "l102"
+            ],
+            "createdAt": 1590999730348,
+            "dueDate": 16156215211,
+            "byMember": {
+              "_id": "u101",
+              "username": "Tal",
+              "fullname": "Tal Tarablus",
+              "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+            },
+            "style": {
+              "bgColor": "#26de81"
+            }
+          }
+        ],
+        "style": {}
+      }
+    ],
+    "activities": [
+      {
+        "id": "a101",
+        "txt": "Changed Color",
+        "createdAt": 154514,
+        "byMember": {
+          "_id": "u101",
+          "fullname": "Abi Abambi",
+          "imgUrl": "http://some-img"
+        },
+        "task": {
+          "id": "c101",
+          "title": "Replace Logo"
+        }
+      }
+    ],
+    "cmpsOrder": [
+      "member",
+      "status",
+      "priority",
+      "attachments",
+      "timeline"
+    ]
+  },
+  {
+    "_id": "b102",
+    "title": "New dev",
+    "archivedAt": 1589983468418,
+    "createdAt": 1589983468418,
+    "createdBy": {
+      "_id": "u101",
+      "fullname": "Abi Abambi",
+      "imgUrl": "http://some-img"
+    },
+    "style": {},
+    "labels": [
+      {
+        "id": "l101",
+        "title": "Done",
+        "color": "--status-clr1"
+      },
+      {
+        "id": "l102",
+        "title": "Progress",
+        "color": "--status-clr1"
+      }
+    ],
+    "members": [
+      {
+        "_id": "u101",
+        "fullname": "Tal Tarablus",
+        "imgUrl": "https://www.google.com"
+      }
+    ],
+    "groups": [
+      {
+        "id": "g101",
+        "title": "Group 1",
+        "archivedAt": 1589983468418,
+        "tasks": [
+          {
+            "id": "c101",
+            "title": "Replace logo"
+          },
+          {
+            "id": "c102",
+            "title": "Add Samples"
+          }
+        ],
+        "style": {}
+      },
+      {
+        "id": "g102",
+        "title": "Group 2",
+        "tasks": [
+          {
+            "id": "c103",
+            "title": "Do that",
+            "archivedAt": 1589983468418
+          },
+          {
+            "id": "c104",
+            "title": "Help me",
+            "status": "in-progress",
+            "description": "description",
+            "comments": [
+              {
+                "id": "ZdPnm",
+                "txt": "also @yaronb please CR this",
+                "createdAt": 1590999817436,
+                "byMember": {
+                  "_id": "u101",
+                  "fullname": "Tal Tarablus",
+                  "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                }
+              }
+            ],
+            "checklists": [
+              {
+                "id": "YEhmF",
+                "title": "Checklist",
+                "todos": [
+                  {
+                    "id": "212jX",
+                    "title": "To Do 1",
+                    "isDone": false
+                  }
+                ],
+                "createdAt": 1590999817436,
+                "byMember": {
+                  "_id": "u101",
+                  "fullname": "Tal Tarablus",
+                  "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+                }
+              }
+            ],
+            "memberIds": [
+              "u101"
+            ],
+            "labelIds": [
+              "l101",
+              "l102"
+            ],
+            "createdAt": 1590999730348,
+            "dueDate": 16156215211,
+            "byMember": {
+              "_id": "u101",
+              "username": "Tal",
+              "fullname": "Tal Tarablus",
+              "imgUrl": "http://res.cloudinary.com/shaishar9/image/upload/v1590850482/j1glw3c9jsoz2py0miol.jpg"
+            },
+            "style": {
+              "bgColor": "#26de81"
+            }
+          }
+        ],
+        "style": {}
+      }
+    ],
+    "activities": [
+      {
+        "id": "a101",
+        "txt": "Changed Color",
+        "createdAt": 154514,
+        "byMember": {
+          "_id": "u101",
+          "fullname": "Abi Abambi",
+          "imgUrl": "http://some-img"
+        },
+        "task": {
+          "id": "c101",
+          "title": "Replace Logo"
+        }
+      }
+    ],
+    "cmpsOrder": [
+      "member",
+      "status",
+      "priority",
+      "attachments",
+      "timeline"
+    ]
+  }
+]
+
 const STORAGE_KEY = 'boardDB'
 //?- Prod:
 // const BASE_URL = 'board/'
@@ -20,7 +300,7 @@ function query(filterBy) {
     .then((boards) => {
       if (!boards || boards.length === 0) {
         boards = gBoards
-        storageService.postMany(STORAGE_KEY, boards)
+        storageService.postMany(STORAGE_KEY, gBoards)
       }
       return boards
     })
@@ -33,6 +313,13 @@ function query(filterBy) {
 function getById(boardId) {
   //?- Dev:
   return storageService.get(STORAGE_KEY, boardId)
+    .then((board) => {
+      if (!board) {
+        board = gBoards.find(board => board.id === boardId)
+        storageService.postMany(STORAGE_KEY, gBoards)
+      }
+      return board
+    })
   //?- Prod:
   // return httpService.get(BASE_URL + boardId)
 }
