d5201cf9f77f0026f3e0b28eb60b1e066ee5f061
Gal Wender <galwender@gmail.com>
2022-09-17 23:24:13 +0300

    working status

--- src/cmps/task-status-menu.jsx
+++ src/cmps/task-status-menu.jsx
@@ -1,22 +1,29 @@
 import { useDispatch } from 'react-redux'
+import { updateTask } from '../store/board/board.action'
 // import { ReactComponent as Trash } from '../assets/icons/trash-icon.svg'
 // import { removeTask } from '../store/board/board.action'
 
-export const TaskStatusMenu = ({taskId,groupId,boardId}) => {
+export const TaskStatusMenu = ({task,groupId,boardId}) => {
     const dispatch = useDispatch()
     // const onRemoveTask =() => {
     //     const removeObj = {taskId,groupId,boardId}
     //     dispatch(removeTask(removeObj))
         
     // }
+    const onUpdateStatus = (status) => {
+        console.log(task)
+        const updatedTask = {...task,status}
+        console.log(updatedTask)
+        dispatch(updateTask({task:updatedTask,groupId,boardId}))
+    }
 
 return <section className="task-status-menu">
-    <button className='btn-status working-on-it'>Working on it</button>
-    <button className='btn-status stuck'>Stuck</button>
-    <button className='btn-status done'>Done</button>
-    <button className='btn-status waiting-for-qa'>Waiting for QA</button>
-    <button className='btn-status need-help'>Need help</button>
-    <button className='btn-status pending'>Pending</button>
-    <button className='btn-status empty-status'></button>
+    <button className='btn-status working-on-it' onClick={()=>onUpdateStatus('Working on it')}>Working on it</button>
+    <button className='btn-status stuck' onClick={()=>onUpdateStatus('Stuck')}>Stuck</button>
+    <button className='btn-status done' onClick={()=>onUpdateStatus('Done')}>Done</button>
+    <button className='btn-status waiting-for-qa' onClick={()=>onUpdateStatus('Waiting for QA')}>Waiting for QA</button>
+    <button className='btn-status need-help' onClick={()=>onUpdateStatus('Need help')}>Need help</button>
+    <button className='btn-status pending' onClick={()=>onUpdateStatus('Pending')}>Pending</button>
+    <button className='btn-status empty-status' onClick={()=>onUpdateStatus('')}></button>
 </section>
 }
