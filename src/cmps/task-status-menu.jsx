e1ee9f81805f5fe488c866280e3003ac95446a27
Gal Wender <galwender@gmail.com>
2022-09-17 19:25:26 +0300

    working on task status

---
+++ src/cmps/task-status-menu.jsx
@@ -0,0 +1,16 @@
+import { useDispatch } from 'react-redux'
+// import { ReactComponent as Trash } from '../assets/icons/trash-icon.svg'
+// import { removeTask } from '../store/board/board.action'
+
+export const TaskStatusMenu = ({taskId,groupId,boardId}) => {
+    const dispatch = useDispatch()
+    // const onRemoveTask =() => {
+    //     const removeObj = {taskId,groupId,boardId}
+    //     dispatch(removeTask(removeObj))
+        
+    // }
+
+return <section className="task-status-menu">
+    
+</section>
+}
