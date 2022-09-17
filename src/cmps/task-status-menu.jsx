4c9c4e76e4eaaef3b733cbc5659f47f879744e87
Gal Wender <galwender@gmail.com>
2022-09-17 20:14:55 +0300

    adding status

--- src/cmps/task-status-menu.jsx
+++ src/cmps/task-status-menu.jsx
@@ -11,6 +11,12 @@ export const TaskStatusMenu = ({taskId,groupId,boardId}) => {
     // }
 
 return <section className="task-status-menu">
-    
+    <button className='btn-status working-on-it'>Working on it</button>
+    <button className='btn-status stuck'>Stuck</button>
+    <button className='btn-status done'>Done</button>
+    <button className='btn-status waiting-for-qa'>Waiting for QA</button>
+    <button className='btn-status need-help'>Need help</button>
+    <button className='btn-status pending'>Pending</button>
+    <button className='btn-status empty-status'></button>
 </section>
 }
