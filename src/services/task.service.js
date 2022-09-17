a0082f8f43c2814ede9a58ad919481438096db39
or-agami <oragami0@gmail.com>
2022-09-17 15:17:44 +0300

    fix get group by id

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -12,7 +12,7 @@ export const taskService = {
 
 async function getById({ taskId, groupId, boardId }) {
   //?- Dev:
-  const group = await groupService.getById(groupId, boardId)
+  const group = await groupService.getById({groupId, boardId})
   const task = group.tasks.find((t) => t.id === taskId)
   return task
   //?- Prod:
@@ -21,7 +21,7 @@ async function getById({ taskId, groupId, boardId }) {
 
 async function remove({ taskId, groupId, boardId }) {
   //?- Dev:
-  const group = await groupService.getById(groupId, boardId)
+  const group = await groupService.getById({groupId, boardId})
   group.tasks = group.tasks.filter((t) => t.id !== taskId)
   return groupService.update({group, boardId})
   //?- Prod:
@@ -29,14 +29,14 @@ async function remove({ taskId, groupId, boardId }) {
 }
 
 async function update({ task, groupId, boardId }) {
-  const group = await groupService.getById(groupId, boardId)
+  const group = await groupService.getById({groupId, boardId})
   // Todo: add user activity to the task
   group.tasks = group.tasks.map((t) => (t.id !== task.id) ? t : task)
   return groupService.update({group, boardId})
 }
 
 async function save({ title, groupId, boardId }) {
-  const group = await groupService.getById(groupId, boardId)
+  const group = await groupService.getById({groupId, boardId})
   // Todo: add user activity to the task
   console.log('title from taskService:', title)
   const task = { id: utilService.makeId(), title }
