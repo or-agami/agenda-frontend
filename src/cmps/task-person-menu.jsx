bafe53134445b36b692f8be1708c3b934d1b282b
Gal Wender <galwender@gmail.com>
2022-09-18 16:29:45 +0300

    added removing of members from task

--- src/cmps/task-person-menu.jsx
+++ src/cmps/task-person-menu.jsx
@@ -17,8 +17,21 @@ export const TaskPersonMenu = ({ task, groupId, board, setIsTaskPersonMenuOpen }
         else {
             updatedTask = { ...task, memberIds: [member._id] }
         }
-        dispatch(updateTask({task:updatedTask,groupId,boardId:board._id}))
+        dispatch(updateTask({ task: updatedTask, groupId, boardId: board._id }))
         setIsTaskPersonMenuOpen(false)
+        return
+    }
+
+    const removeMemberFromTask = (member) => {
+        let updatedTask
+        if (task.memberIds) {
+            if (task.memberIds.includes(member._id)) {
+                updatedTask = { ...task, memberIds: task.memberIds.filter(memberId => memberId !== member._id) }
+                dispatch(updateTask({ task: updatedTask, groupId, boardId: board._id }))
+                setIsTaskPersonMenuOpen(false)
+            }
+        }
+        return
     }
 
     return <section className="task-person-menu">
@@ -26,7 +39,7 @@ export const TaskPersonMenu = ({ task, groupId, board, setIsTaskPersonMenuOpen }
             return <div key={member._id} className="member-container" onClick={() => addMemberToTask(member)}>
                 <img src={require(`../assets/img/${member.imgUrl}.png`)} alt="" />
                 <h4>{member.fullname}</h4>
-                <button className="btn-remove-person-from-task">x</button>
+                <button className="btn-remove-person-from-task" onClick={() => removeMemberFromTask(member)}>x</button>
             </div>
         })}
     </section>
