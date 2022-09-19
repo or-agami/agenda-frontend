06fa6d1283ae8cecdeb7d2c2f9218c0b8d5de0c8
or-agami <oragami0@gmail.com>
2022-09-19 17:10:05 +0300

    user home name update

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, compose, legacy_createStore as create
 
 import thunk from 'redux-thunk'
 import { boardReducer } from './board/board.reducer'
+import { userReducer } from './user/user.reducer'
 
 
 const rootReducer = combineReducers({
     boardModule: boardReducer,
+    userModule: userReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
