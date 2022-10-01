bf10d1eb3fe7fc0a2c5db806157421bd56c24c7e
TalBenAtiya <benatia.tal@gmail.com>
2022-10-01 17:04:26 +0200

    dashboard updates

--- src/views/dashboard.jsx
+++ src/views/dashboard.jsx
@@ -5,6 +5,10 @@ import { Doughnut, Bar, Line } from "react-chartjs-2"
 import { Chart as ChartJS, ArcElement, Tooltip, LineElement, Legend, CategoryScale, LinearScale, BarElement, Title, PointElement } from "chart.js"
 import { useEffect, useState } from "react"
 import { taskService } from "../services/task.service"
+import { BsFillHddStackFill } from 'react-icons/bs'
+import { BsPeopleFill } from 'react-icons/bs'
+import { FaCheckCircle } from 'react-icons/fa'
+
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LineElement, LinearScale, BarElement, Title, PointElement)
 
@@ -13,44 +17,13 @@ export const Dashboard = () => {
     const board = useSelector(state => state.boardModule.board)
     const priorityOpts = ['critical', 'low', 'medium', 'high', '']
     const statusOpts = ['done', 'working on it', 'stuck', 'need help', 'waiting for qa', 'pending', '']
-    const [statusCounter, setStatusCounter] = useState()
-    const [priorityCounter, setPriorityCounter] = useState()
+    const [dataCounter, setDataCounter] = useState(null)
+    const [tasks, setTasks] = useState(null)
 
     useEffect(() => {
-        getTablesData()
+        getTablesData(statusOpts, priorityOpts, board, setDataCounter, setTasks)
     }, [board])
 
-    const getTablesData = async () => {
-        if (!board) return
-        let priorityCounts = []
-        let statusCounts = []
-        const newTasks = await taskService.query({ boardId: board._id })
-        statusOpts.forEach((status, idx) => {
-            statusCounts[idx] = 0
-            newTasks.forEach(task => {
-                if (task.status) {
-                    if (task.status.toLowerCase() === status) statusCounts[idx] += 1
-                } else if (!task.status) {
-                    statusCounts[statusOpts.length - 1] += 1
-                }
-            })
-        })
-
-        setStatusCounter(statusCounts)
-
-        priorityOpts.forEach((priority, idx) => {
-            priorityCounts[idx] = 0
-            newTasks.forEach(task => {
-                if (task.priority) {
-                    if (task.priority.toLowerCase() === priority) priorityCounts[idx] += 1
-                } else if (!task.priority) {
-                    priorityCounts[priorityOpts.length - 1] += 1
-                }
-            })
-        })
-
-        setPriorityCounter(priorityCounts)
-    }
 
     const doungnutData = {
         labels: board?.groups.map(group => (group.title)),
@@ -100,7 +73,7 @@ export const Dashboard = () => {
         datasets: [
             {
                 label: 'Status count',
-                data: statusCounter,
+                data: dataCounter?.status,
                 backgroundColor: [
                     "#00c875",
                     "#fdab3d",
@@ -128,10 +101,6 @@ export const Dashboard = () => {
                     pointStyle: 'circle',
                 }
             },
-            title: {
-                display: true,
-                text: 'Chart',
-            },
         },
     };
 
@@ -141,7 +110,7 @@ export const Dashboard = () => {
             {
                 fill: true,
                 label: 'Priority count',
-                data: priorityCounter,
+                data: dataCounter?.priority,
                 borderColor: [
                     "#333333",
                     "#579bfc",
@@ -161,27 +130,133 @@ export const Dashboard = () => {
         ],
     }
 
-    if (!board) return <Loader />
+
+
+    if (!board || !tasks) return <Loader />
+    const statusStat = StatusStat(tasks)
     return <section className="dashboard">
         <BoardHeader board={board} />
-        <div className="dashboard-main-container">
-            <div className="table-chart">
-                <h3 className="chart-title">Task Per Group</h3>
-                <Doughnut className="chart" data={doungnutData} options={options} />
+
+        <section className="dashboard-info">
+            <div className="top-dashboard-item">
+                <div className="icon groups"><BsFillHddStackFill /></div>
+                <div className="flex">
+                    <span><span className="info-number">{board.groups.length}</span> Groups</span>
+                </div>
             </div>
+            <div className="top-dashboard-item">
+                <div className="icon tasks"><FaCheckCircle /></div>
+                <div className="flex">
+                    <span><span className="info-number">{tasks.length}</span> Tasks</span>
+                    <div className="task-count">{statusStat.doneCount + " Done"}</div>
+                </div>
+            </div>
+            <div className="top-dashboard-item">
+                <div className="icon members"><BsPeopleFill /></div>
+                <div className="flex">
+                    <span><span className="info-number">{board.members.length}</span> Developers</span>
+                     <div className="members-container">{board.members.map(member => {
+                        return <img key={member._id} src={require(`../assets/img/${member.imgUrl}.png`)}/>
+                     })}</div>
+                </div>
+            </div>
+        </section>
+
+        <div className="dashboard-main-container">
 
             <div className="table-chart">
                 <h3 className="chart-title">Tasks Status</h3>
-                <Bar className="chart" data={statusData} options={options} />
+                <div className="chart-container">
+                <div className="status-battery">
+                    {statusStat.elStatus}
+                {(statusStat.doneCount / tasks.length * 100).toFixed(1) + "% Done"}
+                </div>
+                </div>
+            </div>
+
+            <div className="table-chart">
+                <h3 className="chart-title">Task Per Group</h3>
+                <div className="chart-container">
+                <Doughnut className="chart" data={doungnutData} options={options} />
+                </div>
             </div>
 
             <div className="table-chart">
                 <h3 className="chart-title">Tasks Priority</h3>
+                <div className="chart-container">
                 <Line className="chart" data={priorityData} options={options} />
+                </div>
             </div>
         </div>
 
 
 
     </section>
+}
+
+const StatusStat = (tasks) => {
+    const getStatusProgressBar = () => {
+        const statusProgressBar = []
+        let counter = {}
+        let doneCount = 0
+        tasks.forEach(({ status }) => {
+            counter[status] = (counter[status] || 0) + 1
+
+        })
+        let forInCounter = 0
+        for (const status in counter) {
+            forInCounter++
+            const counts = counter[status]
+            if (status === 'Done') doneCount = counts
+            statusProgressBar.push(
+                <div key={forInCounter} className={`status-progress ${status === 'undefined' ? 'none' : makeClass(status)}`}
+                    style={{ width: `${counts / tasks.length * 100}%` }}
+                    title={`${status === 'undefined' ? 'none' : status}: ${(counts / tasks.length * 100).toFixed()}%`}>
+                </div>
+            )
+        }
+        return { statusProgressBar, doneCount }
+    }
+    const statusProgress = getStatusProgressBar()
+    return {
+        elStatus: <div className="status-progress-bar">{statusProgress.statusProgressBar}</div>,
+        doneCount: statusProgress.doneCount
+    }
+}
+
+const makeClass = (status) => {
+    if (!status) return
+    return status.split(' ').join('')
+}
+
+const getTablesData = async (statusOpts, priorityOpts, board, setDataCounter, setTasks) => {
+    if (!board) return
+    let priorityCounts = []
+    let statusCounts = []
+    const newTasks = await taskService.query({ boardId: board._id })
+
+    statusOpts.forEach((status, idx) => {
+        statusCounts[idx] = 0
+
+        newTasks.forEach(task => {
+            if (task.status) {
+                if (task.status.toLowerCase() === status) statusCounts[idx] += 1
+            } else if (!task.status) {
+                statusCounts[statusOpts.length - 1] += 1
+            }
+        })
+    })
+
+    priorityOpts.forEach((priority, idx) => {
+        priorityCounts[idx] = 0
+        newTasks.forEach(task => {
+            if (task.priority) {
+                if (task.priority.toLowerCase() === priority) priorityCounts[idx] += 1
+            } else if (!task.priority) {
+                priorityCounts[priorityOpts.length - 1] += 1
+            }
+        })
+    })
+    setDataCounter({ status: statusCounts, priority: priorityCounts })
+    setTasks(newTasks)
 }
